8995bf2e4245f3e1b19eb51de4591ad5d9f3f8c9
kushwaha Bhavesh <bhavesh18122002@gmail.com>
2024-05-06 10:32:29 +0530

    final Deploy: typing animation

--- src/Component/Hero/Hero.jsx
+++ src/Component/Hero/Hero.jsx
@@ -4,23 +4,48 @@ import Profile from "/assets/img/profile.png"
 import Html from "/assets/img/html-icon.png"
 import Css from "/assets/img/css-icon.png"
 import Javascript from "/assets/img/javascript-icon.png"
-
+import Typed from "typed.js";
 import Node from "/assets/img/node-logo.png"
-import { Fade} from 'react-awesome-reveal'
+import { Fade } from 'react-awesome-reveal'
+import { useEffect } from 'react'
+
 
 
 const Hero = () => {
+
+  useEffect(() => {
+    const typed = new Typed('.typed-animation', {
+      strings: ["<span  style='color:lime'>Front-End</span> Developer", "<span style='color:chartreuse'>MERN</span> Developer",],
+      typeSpeed: 50,
+      smartBackspace: true,
+      backSpeed: 50,
+      loop: true,
+      showCursor: true,
+      cursorChar: '|',
+      autoInsertCss: true,
+      bindInputFocusEvents: false,
+
+    })
+
+    return () => {
+      typed.destroy();
+    }
+  }, [])
+
   return <>
     <main>
       <section className='hero-container' id='home'>
         <div className='hero-content'>
-          <Fade duration={2000} direction='up' cascade='1.1' triggerOnce={true} >
-            <h2>" Crafting   Experiences , Shaping Dreams " </h2>
-            <p className='slogan'>Your Frontend Journey Begins Here!s</p>
+          <Fade duration={2000} cascade='1.1' triggerOnce={true} >
+            <span> Hii</span><h2>I am Kushwaha Bhavesh</h2>
+            <span className='typed-animation'></span>
+            <p className='slogan'>" Crafting   Experiences , Shaping Dreams " </p>
             <p className='descriptions'>"Crafting Tomorrow's Digital Experiences: Where Code Converges with Creativity to Shape Seamless User Interfaces."</p>
+            
+            
           </Fade>
         </div>
-        <Fade  duration={2000} triggerOnce={true}>
+        <Fade duration={2000} triggerOnce={true}>
           <div className='hero-img'>
             <div>
               <div className='top-img'>
